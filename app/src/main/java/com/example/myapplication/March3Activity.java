98a59d891eb9aafbdfea4ec4dcd00997e3b249ad
Archie Bontog <bontogarchie@gmail.com>
2023-03-04 13:05:00 +0800

    ARCHIE BONTOG

--- app/src/main/java/com/example/myapplication/March3Activity.java
+++ app/src/main/java/com/example/myapplication/March3Activity.java
@@ -3,6 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class March3Activity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class March3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_march3);
+
+        Spinner spinnerChoices = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.choices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinnerChoices.setAdapter(adapter);
     }
 }
