5f1a22e76e9e8330352b6e66ed966494b0fdce60
Archie Bontog <bontogarchie@gmail.com>
2023-03-04 11:45:33 +0800

    ARCHIE BONTOG

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            Log.v(TAG,"This is a verbose log.");
+            Log.d(TAG,"This is a debug log.");
+            Log.i(TAG,"This is an info log.");
+            Log.w(TAG, "This is a warn log.");
+            Log.e(TAG,"This is an error log");
+    }
+}
