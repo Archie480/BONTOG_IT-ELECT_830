b9a266b77e35897cea526ae4b91326585be8b546
Archie480 <bontogarchie@gmail.com>
2023-03-22 00:40:19 +0800

    First Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -22,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG,"This is an info log.");
             Log.w(TAG, "This is a warn log.");
             Log.e(TAG,"This is an error log");
+
+            Button button = (Button) findViewById(R.id.btnLinearLayout);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i(TAG, "Button Clicked");
+                    Intent intent = new Intent( MainActivity.this, LinearLayout.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
