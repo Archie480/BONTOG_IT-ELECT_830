90f51c59efe55fe118d6d61e5000729ceb226511
Admin <bontogarchie@gmail.com>
2023-04-15 12:40:15 +0800

    04-15-23 ACTIVITY

---
+++ bottomnav2/src/main/java/com/example/bottomnav2/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.bottomnav2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView = (TextView) findViewById(R.id.text_second);
+
+
+
+    }
+}
